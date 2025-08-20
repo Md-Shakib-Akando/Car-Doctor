2166771dfc062a92cca14ad9c7ee17927b34c5eb
Md-Shakib-Akando <mdshakibss80792@gmail.com>
2025-08-21 03:10:07 +0600

    design frontend and connect mongodb for data facing

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="  bg-base-100 shadow-sm">
+                <div className='max-w-11/12 mx-auto navbar'>
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow space-x-6">
+                                <Link href="/">Home</Link>
+                                <Link href="/product">Product</Link>
+
+                            </ul>
+                        </div>
+                        <Link href="/">
+                            <img src={"/assets/logo.svg"} alt="" />
+                        </Link>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 space-x-6 text-lg font-semibold " >
+                            <Link href="/" className='hover:text-[#FF3811]'>Home</Link>
+                            <Link href="/product" className='hover:text-[#FF3811]'>Product</Link>
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <Link href={'/login'} className="btn bg-white text-[#FF3811] text-lg border-[#FF3811] hover:bg-[#FF3811] hover:text-white rounded">Login</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/app/Components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div className="max-w-11/12 mx-auto">
+            <div
+                className="hero h-[80vh] rounded-xl"
+                style={{
+                    backgroundImage:
+                        "url('/assets/images/banner/4.jpg')",
+                }}
+            >
+                <div className="hero-overlay bg-black/60 "></div>
+                <div className="hero-content  text-neutral-content text-center">
+                    <div className="">
+                        <h1 className="mb-7 text-5xl font-bold">Affordable Price For Car Servicing</h1>
+                        <p className="mb-7 text-lg text-center">
+                            There are many variations of passages of  available,<br /> but the majority have suffered alteration in some form
+                        </p>
+                        <button className="btn bg-[#FF3811] text-white border-0 shadow-none">Discover More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
